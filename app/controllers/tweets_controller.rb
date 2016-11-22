cfdc4397d620c9e97c4e63dbc4faf8f372b46173
山岡 佑 <tasuku.yamaoka@gmail.com>
2016-11-22 19:00:23 +0900

    投稿画面の作成完了

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,7 +4,15 @@ class TweetsController < ApplicationController
   end
 
   def new
-    
+  end
+
+  def create
+    Tweet.create(tweet_params)
+  end
+
+  private
+  def tweet_params
+    params.permit(:name,:image,:text)
   end
 
 end
